40d23bf16741207c4ec2639cd9a41f5409c323c4
mcheruku <30708729+mcheruku@users.noreply.github.com>
2018-05-25 15:22:41 -0400

    Adress change

--- sampledb/Tables/Person.Address.sql
+++ sampledb/Tables/Person.Address.sql
@@ -1,8 +1,8 @@
 CREATE TABLE [Person].[Address]
 (
-[AddressID] [int] NOT NULL,
-[AddressLine1] [nvarchar] (60) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
-[AddressLine2] [nvarchar] (60) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[Addressid] [int] NOT NULL,
+[AddressL1] [nvarchar] (60) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[AddressL2] [nvarchar] (60) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [City] [nvarchar] (30) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [StateProvinceID] [int] NOT NULL,
 [PostalCode] [nvarchar] (15) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,

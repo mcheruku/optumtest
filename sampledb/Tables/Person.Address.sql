1e0fb7af5cd5980f4d388ba7eb39245c73d41570
mcheruku <30708729+mcheruku@users.noreply.github.com>
2018-05-29 22:17:28 -0400

    mycommit

--- sampledb/Tables/Person.Address.sql
+++ sampledb/Tables/Person.Address.sql
@@ -1,11 +1,11 @@
 CREATE TABLE [Person].[Address]
 (
-[AddressID] [int] NOT NULL,
+[AddressId] [int] NOT NULL,
 [AddressL1] [nvarchar] (60) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [AddressL2] [nvarchar] (60) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [City] [nvarchar] (30) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [StateProvinceid] [int] NOT NULL,
-[PostalCode] [nvarchar] (15) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[postalcode1] [nvarchar] (15) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [SpatialLocation] [sys].[geography] NULL,
 [rowguid] [uniqueidentifier] NOT NULL,
 [ModifiedDate] [datetime] NOT NULL

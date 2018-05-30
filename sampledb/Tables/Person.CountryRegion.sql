98bb94af4b445a33bf8a9c6217bbdd84f344a544
mcheruku <30708729+mcheruku@users.noreply.github.com>
2018-05-29 21:30:16 -0400

    codechanges

--- sampledb/Tables/Person.CountryRegion.sql
+++ sampledb/Tables/Person.CountryRegion.sql
@@ -1,6 +1,6 @@
 CREATE TABLE [Person].[CountryRegion]
 (
-[CountryRegionCode] [nvarchar] (3) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[CountryRegionCode1] [nvarchar] (3) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [Name] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [ModifiedDate] [datetime] NOT NULL
 ) ON [PRIMARY]
